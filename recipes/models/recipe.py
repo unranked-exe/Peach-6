71fd3830407aafe48c0debd006733af00017f3fc
unranked-exe <120649405+unranked-exe@users.noreply.github.com>
2025-11-13 18:41:31 +0000

    test(recipe): Added tests to recipe model
    Created a recipe fixture (valid_recipe.json) to use in set_up method of all tests in Recipe.
    
    Added tests which test the validity of attributes in Recipe Model
    
    Also altered model to prevent certain fields being left out (blanked)

--- recipes/models/recipe.py
+++ recipes/models/recipe.py
@@ -3,17 +3,17 @@ from .user import User
 
 class Recipe(models.Model):
 
-    name = models.CharField(max_length=100)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
-    ingredients = models.TextField()
-    instructions = models.TextField()
+    name = models.CharField(max_length=100, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes', blank=False)
+    ingredients = models.TextField(blank=False)
+    instructions = models.TextField(blank=False)
     date_created = models.DateTimeField(auto_now_add=True)
-    difficulty_level = models.CharField(max_length=50)
-    preparation_time_mins = models.IntegerField(help_text="Preparation time in minutes")
+    difficulty_level = models.CharField(max_length=50, blank=False)
+    preparation_time_mins = models.IntegerField(help_text="Preparation time in minutes", blank=False)
 
 #Sample init 
 # user1 = User.objects.first()
-# recipe1 = Recipe(name = "Lasagna", author=user1,ingredients = "Ingredients", instructions = "Sample Instructions", difficulty_level = "Easy", preparation_time_mins = 30)
+# recipe1 = Recipe(name = "Lasagna", author=user1, ingredients = "Ingredients", instructions = "Sample Instructions", difficulty_level = "Easy", preparation_time_mins = 30)
 
     def __str__(self):
         return f"{self.name} by {self.author.username}"
