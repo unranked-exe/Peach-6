83993729bbd4718faaebf6999e63ca31c246ea75
Qozfa Hussain <qozfa.hussain@kcl.ac.uk>
2025-11-15 18:18:24 +0000

    Fix foodtag tests to correct field names

--- recipes/tests/models/test_foodtag.py
+++ recipes/tests/models/test_foodtag.py
@@ -19,15 +19,15 @@ class FoodTagTestCase(TestCase):
 
     def test_max_tag_name_length_cannot_be_more_than_50_chars(self):
         self.foodtag.tag_name = 'q' * 51
-        self._assert_tag_name_is_invalid()
+        self._assert_foodtag_is_invalid()
 
     def test_max_tag_name_length_can_be_50_chars(self):
         self.foodtag.tag_name = 'q' * 50
-        self._assert_tag_name_is_valid()
+        self._assert_foodtag_is_valid()
     
     def test_foodtag_tag_name_can_be_blank(self):
         self.foodtag.tag_name = ''
-        self._assert_recipe_is_valid()
+        self._assert_foodtag_is_valid()
 
     def _assert_foodtag_is_valid(self):
         try:
